742b6dd6afaadba10b6569f93262915b69699a89
Thanaphum Sinthurak <autolovemy@gmail.com>
2019-08-24 00:32:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/mytodo/SplashActivity.java
@@ -0,0 +1,29 @@
+package com.example.mytodo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //todo: โค้ดที่ต้องการให้ทำงานจาก delay ครบ 3 นาที
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+
+    }
+}
